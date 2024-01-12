6ecb9a519c52bffee154354f7121724631f6a5a6
Vimaser <thfeske@gmail.com>
2024-01-11 18:09:34 -0600

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,56 @@
+import React from "react";
+import Footer from "./Footer";
+import "./css/About.css";
+
+function About() {
+  return (
+    <>
+      <div className="about-container mt-5">
+        <h1>Meet Dan Broe: A Life in Music</h1>
+        <p>
+          From the lively streets of Seattle to the soulful corners of San
+          Francisco, Dan Broe began his musical journey, strumming his way into
+          the hearts of the West Coast crowd. But it was in the late 60s, when
+          he set foot in the vibrant city of New Orleans, that Dan truly found
+          his rhythm.
+        </p>
+        <p>
+          For over a decade, Dan became a familiar face in the French Quarter,
+          Fat City, and on the west bank of New Orleans, playing everywhere from
+          hole-in-the-wall joints to the bustling stages of Bourbon Street.
+          Night after night, at the renowned Papa Joe’s, his melodies would
+          blend with the sounds of the city, creating memories that lasted well
+          past the Louisiana moon.
+        </p>
+        <p>
+          But Dan's love for music wasn't just about performing. He honed his
+          craft studying at Loyola University, then at Louisiana State
+          University, deepening his understanding of the notes that had always
+          been his closest friends. His passion for music took a new turn when
+          he stepped into the classroom, teaching in the East Baton Rouge Parish
+          School System for 26 years. Through these years, Dan wasn't just a
+          teacher; he was a mentor, an inspiration, and a guiding light for many
+          young musicians.
+        </p>
+        <p>
+          Even with teaching, Dan never left the stage. He graced venues like
+          Dalton’s in downtown Baton Rouge, and his performances became a staple
+          at La Divina’s every Friday night. His special concert at the Shaw
+          Center and his regular gigs at the Londoner were more than just shows;
+          they were where his heart sang the loudest.
+        </p>
+        <p>
+          Dan Broe's story isn't just a tale of venues and concerts; it's a
+          journey of a man whose guitar was his voice, and whose music became a
+          part of the very soul of every place he played. So, if you find
+          yourself in Baton Rouge on a Friday night, follow the melody to La
+          Divina’s – you'll be in for a musical experience that's as rich and
+          heartwarming as the life Dan Broe has lived.
+        </p>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./css/Home.css";
+import img from "../img/zhomefinal.png";
+import Footer from "./Footer";
+
+function Home() {
+  return (
+    <div className="home-background-image">
+      <div className="container mt-5">
+        {/* Image Section */}
+        <section className="image-section">
+          <img src={img} alt="Dan Broe" />
+        </section>
+
+        {/* Upcoming Events Section */}
+{/*         <section>
+          <div class="box">
+            <Events />
+          </div> 
+        </section>*/}
+      </div>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
